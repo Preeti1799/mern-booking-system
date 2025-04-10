96d9417c5c429449511e8aedb4a25a720008c643
Preeti Paswan <paswanpreeti99@gmail.com>
2025-04-10 15:29:16 +0530

    Force update axios.js and App.js for Vercel deployment

--- client/src/api/axios.js
+++ client/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://mern-booking-system-1.onrender.com/api",
+  baseURL: "https://mern-booking-backend-sov6.onrender.com/api",
 });
 
 export default api;
